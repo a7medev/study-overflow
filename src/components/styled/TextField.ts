e9d292e210d38c3e3cb89b0f83917b838f0326ea
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2021-11-02 08:17:23 +0200

    chore: add TextField component

---
+++ src/components/styled/TextField.ts
@@ -0,0 +1,16 @@
+import styled from 'styled-components/native';
+
+const TextField = styled.TextInput.attrs(props => ({
+  placeholderTextColor: props.theme.colors.secondaryLight,
+}))`
+  padding: 10px 20px;
+  background-color: ${props => props.theme.colors.white};
+  color: ${props => props.theme.colors.black};
+  border: 2px solid ${props => props.theme.colors.secondaryLight};
+  border-radius: 7px;
+  text-align: right;
+  font-family: ${props => props.theme.fonts.regular};
+  font-size: 18px;
+`;
+
+export default TextField;
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Screen from '../components/styled/Screen';
+import TextField from '../components/styled/TextField';
 
 import Typo from '../components/styled/Typo';
 
@@ -7,6 +8,7 @@ const HomeScreen: React.FC = () => {
   return (
     <Screen>
       <Typo variant="h1">الرئيسية</Typo>
+      <TextField placeholder="اكتب هنا..." />
     </Screen>
   );
 };
