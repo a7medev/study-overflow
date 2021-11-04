84e30ce0590d96fc3688c757ab984907c5bbe182
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2021-11-04 11:30:00 +0200

    feat: add validation in AuthScreen

--- src/screens/AuthScreen.tsx
+++ src/screens/AuthScreen.tsx
@@ -1,13 +1,29 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
 import Image from 'react-native-fast-image';
+import * as Yup from 'yup';
 
 import Screen from '../components/styled/Screen';
 import Button from '../components/Button';
-import TextField from '../components/form/TextField/TextField';
 import Typo from '../components/styled/Typo';
+import FormTextField from '../components/form/TextField/FormTextField';
+import Form from '../components/form/Form';
+
+const initialValues = { email: '', password: '' };
+const validationSchema = Yup.object({
+  email: Yup.string()
+    .email('أدخل بريد إلكتروني صالح')
+    .required('هذا الحقل مطلوب'),
+  password: Yup.string()
+    .min(6, 'يجب أن تكون على الأقل ٦ حروف')
+    .required('هذا الحقل مطلوب'),
+});
 
 const AuthScreen: React.FC = () => {
+  const handleSubmit = () => {
+    // TODO: handle submit
+  };
+
   return (
     <Screen style={styles.screen}>
       <Image
@@ -21,19 +37,24 @@ const AuthScreen: React.FC = () => {
         انضم لمجتمع المذاكرة الخاص بنا
       </Typo>
 
-      <TextField placeholder="البريد الإلكتروني" />
-      <TextField
-        placeholder="كلمة المرور"
-        secureTextEntry
-        style={styles.passwordField}
-      />
+      <Form
+        onSubmit={handleSubmit}
+        initialValues={initialValues}
+        validationSchema={validationSchema}>
+        <FormTextField name="email" placeholder="البريد الإلكتروني" />
+        <FormTextField
+          name="password"
+          placeholder="كلمة المرور"
+          secureTextEntry
+        />
 
-      <Button fullWidth style={styles.signInButton}>
-        تسجيل الدخول
-      </Button>
-      <Button variant="secondary" fullWidth>
-        إنشاء حساب
-      </Button>
+        <Button fullWidth style={styles.signInButton}>
+          تسجيل الدخول
+        </Button>
+        <Button variant="secondary" fullWidth>
+          إنشاء حساب
+        </Button>
+      </Form>
     </Screen>
   );
 };
