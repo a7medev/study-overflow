ed17748c704ee84ee891a16633192aacb72f4e70
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2021-11-04 12:24:58 +0200

    chore: forward ref to FormTextField

--- src/components/form/TextField/FormTextField.tsx
+++ src/components/form/TextField/FormTextField.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { TextInputProps } from 'react-native';
+import { TextInput, TextInputProps } from 'react-native';
 import { useField } from 'formik';
 
 import TextField from './TextField';
@@ -10,25 +10,31 @@ interface FormTextFieldProps extends TextInputProps {
   name: string;
 }
 
-const FormTextField: React.FC<FormTextFieldProps> = ({
-  name,
-  placeholderTextColor: _, // It makes a type error
-  ...props
-}) => {
-  const [field, meta, helpers] = useField(name);
+const FormTextField = React.forwardRef<TextInput, FormTextFieldProps>(
+  (
+    {
+      name,
+      placeholderTextColor: _, // It makes a type error
+      ...props
+    },
+    ref,
+  ) => {
+    const [field, meta, helpers] = useField(name);
 
-  return (
-    <FieldContainer>
-      <TextField
-        value={field.value}
-        onBlur={() => helpers.setTouched(true)}
-        onChangeText={text => helpers.setValue(text)}
-        error={meta.touched && !!meta.error}
-        {...props}
-      />
-      <FormError touched={meta.touched} error={meta.error} />
-    </FieldContainer>
-  );
-};
+    return (
+      <FieldContainer>
+        <TextField
+          value={field.value}
+          onBlur={() => helpers.setTouched(true)}
+          onChangeText={text => helpers.setValue(text)}
+          error={meta.touched && !!meta.error}
+          ref={ref}
+          {...props}
+        />
+        <FormError touched={meta.touched} error={meta.error} />
+      </FieldContainer>
+    );
+  },
+);
 
 export default FormTextField;
