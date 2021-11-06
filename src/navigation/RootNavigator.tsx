a1282bd47dff8b79570f9521b054d26a51f907f7
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2021-11-06 16:47:27 +0200

    chore: enable react freeze

--- src/navigation/RootNavigator.tsx
+++ src/navigation/RootNavigator.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { enableFreeze } from 'react-native-screens';
 
 import AuthScreen from '../screens/AuthScreen';
 import HomeNavigator from './HomeNavigator';
 import useWatchAuthState from '../hooks/useWatchAuthState';
 import { useAppSelector } from '../store/hooks';
+
+enableFreeze(true);
 
 const RootNavigator: React.FC = () => {
   const user = useAppSelector(state => state.auth.user);
