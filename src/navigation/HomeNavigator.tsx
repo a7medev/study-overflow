6817081619b3c98c9ce0249de33aa96e42153d1a
Ahmed Mahmoud <a7med.mahmoud2004@gmail.com>
2021-11-06 17:54:05 +0200

    chore: HomeNavigator change to bottom tabs

--- src/navigation/HomeNavigator.tsx
+++ src/navigation/HomeNavigator.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { useTheme } from 'styled-components/native';
 
 import HomeScreen from '../screens/HomeScreen';
 
@@ -7,13 +9,34 @@ export type HomeParamList = {
   Home: undefined;
 };
 
-const Stack = createNativeStackNavigator<HomeParamList>();
+const Tab = createBottomTabNavigator<HomeParamList>();
+
+const icon = (name: string, props: { focused: boolean }) =>
+  props.focused ? name : `${name}-outline`;
 
 const HomeNavigator: React.FC = () => {
+  const theme = useTheme();
+
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Home" component={HomeScreen} />
-    </Stack.Navigator>
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarLabelStyle: {
+          fontFamily: theme.fonts.regular,
+          fontSize: 14,
+        },
+      }}>
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarLabel: 'الرئيسية',
+          tabBarIcon: props => (
+            <Ionicons {...props} name={icon('home', props)} />
+          ),
+        }}
+      />
+    </Tab.Navigator>
   );
 };
 
